6c30363f6ee047c530398f8b8573ecdc4b2ad9d2
Emily <esonthipanyaa@gmail.com>
2020-04-15 20:18:41 -0400

    Added more comments and print out one more random line

--- get-quote.py
+++ get-quote.py
@@ -3,12 +3,18 @@ import random
 def primary():
   # print("Keep it logically awesome.")
 
+  #This will assign the variable "f" to open the specified file.
+  #Then the program will read the entire txt file and store in
+  #into the variable "quotes".
+  #IT IS IMPORTANT TO CLOSE THE FILE EACH TIME.
+  
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
   last =13
   rnd = random.randint(0, last)
+  print(quotes[rnd])
   print(quotes[rnd])
 
 if __name__== "__main__":
